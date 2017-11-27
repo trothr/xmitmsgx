0ad39e2a8cb66b815b2222afbb30e635ef0a3fd0
Rick Troth <trothr@gmail.com>
2017-11-27 09:29:29 -0500

    more tinkering with the header ... and some canned tests

--- xmitmsgx.h
+++ xmitmsgx.h
@@ -26,6 +26,41 @@
 #define  MSGFLAG_NOPRINT  0x04
 
 
+typedef struct MSGSTRUCT
+  {
+    int  msgnum;        /* message number */
+    int  msgc;          /* count of replacement tokens */
+    char **msgv;        /* vector of replacement tokens */
+    unsigned char *msgbuf;   /* supplied by caller */
+    int  msglen;        /* buffer size on input, msg length on return */
+    unsigned char *msgtext;   /* offset past msg code/header */
+
+    int  msgfmt;        /* message format number (for future use) */
+    int  msgline;       /* message line number (for future use, zero means all) */
+    int  msglevel;      /* message level/serverity (zero means use the letter in the file) */
+
+    /* the following are probably not for external use */
+    char *applid;       /* default is basename of messages file, optional SYSLOG identity */
+    char *caller;       /* default is getenv("LOGNAME") roughly, msgu */
+    char *prefix;       /* default is applid[0..2]||caller[0..2] */
+    char *letter;       /* default taken from message file */
+
+    /* the following are filled in by msgopen() not for external use */
+    int  msgmax;        /* highest message number in table */
+    char **msgtable;    /* array of messages (allocated memory) */
+    char *msgdata;      /* messages file content (allocated memory) */
+    char *msgfile;      /* name of message file found (for debugging) */
+
+    char  pfxa[4];      /* truncated up-cased applid */
+    char  pfxc[4];      /* truncated up-cased caller */
+    char  locale[24];   /* possibly truncated to match the nearest file found */
+
+    int  msgopts;
+    char *escape;
+
+  } MSGSTRUCT;
+
+
 /* Open the messages file, read it, get ready for service. */
 extern int msgopen(const char*,int,struct MSGSTRUCT*);
 /* filename, flags, MSGSTRUCT */
@@ -36,48 +71,10 @@ extern int msgopen(const char*,int,struct MSGSTRUCT*);
 extern int msgmake(struct MSGSTRUCT*);
 
 /* follows snprintf() */
-extern int msgprint(char*,int,int,int,char**,int);
-/* buffer, length, msgnum, msgc, msgv, flag */
+extern int msgprint(int,int,char**,int);
+/* msgnum, msgc, msgv, flag */
 
-extern int msgclose();
-
-typedef struct MSGSTRUCT
-  {
-    int  msgnum;        /* message number */
-    int  msgc;          /* count of replacement tokens */
-    char **msgv;        /* vector of replacement tokens */
-    static unsigned char *msgbuf;   /* supplied by caller */
-    int  msglen;        /* buffer size on input, msg length on return */
-    static unsigned char *msgtext;   /* offset past msg code/header */
-
-    int  msgfmt;        /* message format number (for future use) */
-    int  msgline;       /* message line number (for future use, zero means all) */
-    int  msglevel;      /* message level/serverity (zero means use the letter in the file) */
-
-    char *applid;       /* default is basename of messages file, optional SYSLOG identity */
-    char *caller;       /* default is getenv("LOGNAME") roughly, msgu */
-    char *prefix;       /* default is applid[0..2]||caller[0..2] */
-    char *letter;       /* default taken from message file */
-
-    /* the following are filled in by msgopen() not for user */
-    int  msgmax;        /* highest message number in table */
-    char **msgtable;    /* array of messages (allocated memory) */
-    char *msgdata;      /* messages file content (allocated memory) */
-    char *msgfile;      /* name of message file found (for debugging) */
-
-    char  pfxa[4];      /* truncated up-cased applid */
-    char  pfxc[4];      /* truncated up-cased caller */
-    char  locale[24];   /* possibly truncated to match the nearest file found */
-
-  } MSGSTRUCT;
-
-
-int sprintm(char*,int,char*,char*,int,char,int,char**);
-
-int msgread(char*,int,FILE*,char*,int,char,int,char**);
-
-extern
-FILE *dfopen(char*,char*);
+extern int msgclose(struct MSGSTRUCT*);
 
 #endif
 
